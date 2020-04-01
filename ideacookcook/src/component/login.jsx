f04f75cc3bb8781bdf8180f7282a7ed4323273bf
benzjakk <benzsport357@gmail.com>
2020-04-01 19:56:58 +0700

    create reactapp

---
+++ ideacookcook/src/component/login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Loginer extends Component {
+    
+    render() { 
+        return ( 
+            <div>
+                <h2> Account </h2>
+            </div>
+         );
+    }
+}
+ 
+export default Loginer;
