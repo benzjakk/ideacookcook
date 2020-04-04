0f802e8919c9d4268e0ad691afae0070c607f38d
benzjakk <benzsport357@gmail.com>
2020-04-04 18:50:48 +0700

    delete react-app

--- ideacookcook/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import Loginer from "./component/login";
-
-ReactDOM.render(<Loginer />, document.getElementById("root"));
