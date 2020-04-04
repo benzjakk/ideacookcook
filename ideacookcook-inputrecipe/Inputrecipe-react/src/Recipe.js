dc58d1b5d441b39b2c9f2cfbb5ec5b434d30a220
bestvarut <51030057+bestvarut@users.noreply.github.com>
2020-04-04 22:26:39 +0700

    InputrecipeV1
    use react

---
+++ ideacookcook-inputrecipe/Inputrecipe-react/src/Recipe.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import './Recipe.css'
+
+function Recipe(){
+    return(
+        <section class="inputrecipe">
+            <h1>ลงสูตรอาหาร</h1>
+        <ul>
+        <button id="but1" onclick="document.getElementsByClassName('input1').value = ''" >รีเซ็ท</button>
+        <li>
+        <p>ชื่อเมนูอาหาร : </p>
+        <input type="text" class="input1" />
+        </li>
+        <li>
+                <p>คำอธิบาย : </p>                
+        </li>
+        <textarea rows = "5" cols = "70">
+        </textarea>
+        <li>
+                <p>อัพโหลดรูปภาพ</p>
+        </li>
+        <li>
+                <p>ราคา : </p>
+                <select>
+                    <option value="1">น้อยกว่า 100</option>
+                    <option value="2">100 - 500</option>
+                    <option value="3">มากกว่า 500</option>
+                   
+                </select> 
+                <p>บาท </p>
+        </li>
+        <li>
+                <p>วัตถุดิบ : </p>
+                <input type="text" class="input1"/>
+        </li>
+        <li>
+                <p>อุปกรณ์ : </p>
+                <input type="text" class="input1"/>
+        </li>
+        <li>
+                <p>ขั้นตอน : </p>                
+        </li>
+        <textarea rows = "5" cols = "70" >
+                
+        </textarea>
+        <li>
+                <p>อัพโหลดรูปภาพ</p>
+        </li>
+        <li>
+            <p>ระยะเวลา : </p>
+            <input type="text" class="input1"/>
+        </li>
+        <li>
+            <p>ประเภทอาหาร : </p>
+            <select>
+                <option value="1">คาว</option>
+                <option value="2">หวาน</option>
+                <option value="3">ทะเล</option>
+            </select> 
+        </li>
+        <li>
+                <p>สัญชาติ : </p>
+                <input type="text" class="input1"/>
+        </li>
+        <li>
+                <p>แคลโลรี่ : </p>
+                <input type="text" class="input1"/>
+        </li>
+        <button>ยืนยัน</button>
+
+
+        </ul>
+        </section>
+       
+    );
+}
+
+export default Recipe;
