8d58c3ec099661c266ce5857b8480030a50de6b5
idea2409 <60475612+idea2409@users.noreply.github.com>
2020-05-03 04:11:22 +0700

    แก้

--- ideacookcook/src/function/TimeStamp.js
+++ ideacookcook/src/function/TimeStamp.js
@@ -1,5 +1,4 @@
 import Moment from "moment";
-
 const TimeStamp=(t)=>{
     var a='';
     if(t.substring(4,7)=='Jan')a='01';
@@ -14,7 +13,35 @@ const TimeStamp=(t)=>{
     else if (t.substring(4,7)=='Oct')a='10';
     else if (t.substring(4,7)=='Nov')a='11';
     else if (t.substring(4,7)=='Dec')a='12';
-    return Moment(t.substring(11,15)+a+t.substring(8,10),"YYYYMMDD").fromNow();
+    Moment.updateLocale('en', {
+        relativeTime : {
+            future: "in %s",
+            past:   "%s ago",
+            s  :     'a few sec',
+            ss :    '%d sec',
+            m:       "a min",
+            mm:     "%d min",
+            h:      "an hour",
+            hh:     "%d hours",
+            d:       "a day",
+            dd:     "%d days",
+            w:       "a week",
+            ww:     "%d weeks",
+            M:       "a month",
+            MM:     "%d months",
+            y:       "a year",
+            yy:     "%d years"
+        }
+    });
+    
+    var d=parseInt(t.substring(8,10));
+    var h=parseInt(t.substring(16,18))+7;
+    if(h>=24){h-=24;d+=1}
+    if(h<0)h+=24;
+    if(h<10)h='0'+h;    
+    if(d<10)d='0'+d;
+    var out=Moment(t.substring(11,15)+a+d+h+t.substring(19,21)+t.substring(22,24),"YYYYMMDDhhmmss").fromNow();
+    return(out);
 }
 
 export default TimeStamp;
