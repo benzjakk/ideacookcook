8856647c10fc41ee2946243a5102c00a2b530ae5
benzjakk <benzsport357@gmail.com>
2020-05-02 15:18:11 +0700

    for merge
    จัด folder

--- ideacookinginput/reactapp/src/App.js
+++ ideacookinginput/reactapp/src/App.js
@@ -1,19 +1,17 @@
-import React from 'react';
-import './App.css';
-import Recipe from './Recipe';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Nav from './Nav';
-
+import React from "react";
+import "./App.css";
+import Recipe from "./page/Recipe";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Nav from "./Nav";
 
 function App() {
   return (
     <Router>
-        <div>
-          <Nav />
-          <Route path="/inputrecipe" component={Recipe} />
-        </div> 
+      <div>
+        <Nav />
+        <Route path="/inputrecipe" component={Recipe} />
+      </div>
     </Router>
-    
   );
 }
 
