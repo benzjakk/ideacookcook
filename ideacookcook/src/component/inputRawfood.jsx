5b75faf85765381dab0aa87ca50d3e4018c63c20
benzjakk <benzsport357@gmail.com>
2020-05-07 02:37:40 +0700

    fix bug no rawfood

--- ideacookcook/src/component/inputRawfood.jsx
+++ ideacookcook/src/component/inputRawfood.jsx
@@ -155,10 +155,11 @@ class InputRawFood extends Component {
             </button>
           </span>
         ))}
-        {this.state.raws[this.state.raws.length - 1] !== "" &&
-        this.state.units[this.state.units.length - 1] !== "" &&
-        this.state.quans[this.state.quans.length - 1] !== "" &&
-        parseFloat(this.state.quans[this.state.quans.length - 1]) > 0 ? (
+        {(this.state.raws[this.state.raws.length - 1] !== "" &&
+          this.state.units[this.state.units.length - 1] !== "" &&
+          this.state.quans[this.state.quans.length - 1] !== "" &&
+          parseFloat(this.state.quans[this.state.quans.length - 1]) > 0) ||
+        this.state.raws.length == 0 ? (
           <button onClick={this.addMethod} type="button">
             <b>เพิ่มวัตุดิบ</b>
           </button>
